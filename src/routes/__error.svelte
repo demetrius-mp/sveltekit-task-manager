041ab48b0cf1122a0a6e6d385c15796fef0eeb37
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-02-26 01:28:29 -0400

    feat: add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Button, Container } from 'sveltestrap';
+</script>
+
+<Container>
+	<div class="d-flex flex-column justify-content-center align-items-center min-vh-100 text-center">
+		<h1 class="display-5 fw-bold">Oops</h1>
+		<div class="col-lg-4 mx-auto">
+			<h5>Something went wrong</h5>
+			<p class="lead mb-4">If you typed the URL manually, please check again.</p>
+			<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+				<Button href="/" class="px-4" size="lg" color="primary">Go home</Button>
+			</div>
+		</div>
+	</div>
+</Container>
